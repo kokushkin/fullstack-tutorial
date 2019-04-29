3a4151ff6162c33aa1898a7ba3d2fe61b1e30653
kokushkin <fipsiksi@yandex.ru>
2019-04-29 18:13:40 +0300

    part 7 before Update local data. Problem: detailed launch don't reflect piked item

--- start/client/src/resolvers.js
+++ start/client/src/resolvers.js
@@ -1,4 +1,6 @@
 import gql from 'graphql-tag';
+import { GET_CART_ITEMS } from './pages/cart';
+
 
 export const typeDefs = gql`
   extend type Query {
@@ -15,4 +17,11 @@ export const typeDefs = gql`
   }
 `;
 
-export const resolvers = {};
+export const resolvers = {
+    Launch: {
+        isInCart: (launch, _, { cache }) => {
+          const { cartItems } = cache.readQuery({ query: GET_CART_ITEMS });
+          return cartItems.includes(launch.id);
+        },
+      },
+};
